d5fcc1b62c9aafbb5dfe07c2fa12cefe0c737b22
abm-noman <noman15-2839@diu.edu.bd>
2025-12-07 23:12:59 +0600

    Beginners Projects - 01 Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import Greetings from "./components/Greetings";
-import UserStatus from "./components/UserStatus";
-import Weather from "./components/weather";
+import Counter from "./beginners_projects/01_Counter/Counter";
 
-function App() {
+const App = () => {
 
     return (
         <div>
-            
-            <UserStatus loggedIn = {true} isAdmin={false} />
-            <Weather />
-            <Greetings timeOfDay={""}/>
-
+            <Counter />
         </div>
     );
 }
 
-export default App;
+export default App; 
