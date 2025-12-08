aaef1593046a138db7592dc5139482f3b2accad1
abm-noman <noman15-2839@diu.edu.bd>
2025-12-08 15:20:46 +0600

    ToDo

---
+++ src/beginners_projects/02_ToDo/App.jsx
@@ -0,0 +1,12 @@
+import ToDo from "./beginners_projects/02_ToDo/ToDo";
+
+const App = () => {
+
+    return (
+        <div>
+            <ToDo />
+        </div>
+    );
+}
+
+export default App; 
---
+++ src/beginners_projects/02_ToDo/ToDo.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "../02_ToDo/style.css";
+
+const ToDo = () => {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleSubmit = () => {
+    setTodos((todos) => {
+        return todos.concat({
+        text: input,
+        id: Date.now(),
+      });
+    });
+
+    setInput("");
+  };
+
+  const removeTodo = (id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div className="container">
+      <input
+        type="text"
+        placeholder="New Todo"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Add Todo</button>
+
+      <ul className="todos-list">
+        {todos.map(({ text, id }) => (
+          <li className="todo" key={id}>
+            <span>{text}</span>
+            <button className="close" onClick={() => removeTodo(id)}>
+              x
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ToDo;
