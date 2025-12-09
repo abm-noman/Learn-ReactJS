260c05bba7938dc6fda84fb7dcb5dafaefb5787d
abm-noman <noman15-2839@diu.edu.bd>
2025-12-09 17:17:45 +0600

    Form Validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
-import Accordion from "./beginners_projects/08_Accordions/Accordion.jsx";
-import { accordionData } from "./beginners_projects/08_Accordions/utilities/content.js";
+// import Counter from "./beginners_projects/01_Counter/Counter.jsx";
+// import ToDo from "./beginners_projects/02_ToDoList/ToDo.jsx";
+// import Meals from "./beginners_projects/03_MealsApp/Meals.jsx";
+// import Calculator from "./beginners_projects/04_Calculator/Calculator.jsx";
+// import ToggleBackgroundColor from "./beginners_projects/05_ToggleBackgroundColor/ToggleBackgroundColor.jsx";
+// import HiddenSearchBar from "./beginners_projects/06_HiddenSearchBar/HiddenSearchBar.jsx";
+// import Testimonials from "./beginners_projects/07_Testimonials/Testimonials.jsx";
+// import Accordion from "./beginners_projects/08_Accordions/Accordion.jsx";
+// import { accordionData } from "./beginners_projects/08_Accordions/utilities/content.js";
+import Form from "./beginners_projects/09_Form_Validation/Form.jsx";
+
 const App = () => {
   return (
     <div>
@@ -11,11 +20,15 @@ const App = () => {
       {/* <HiddenSearchBar /> */}
       {/* <Testimonials /> */}
 
-      <div className="accordion">
+      {/* <div className="accordion">
         {accordionData.map(({ title, content }) => (
           <Accordion title={title} content={content} />
         ))}
-      </div>
+      </div> */}
+
+      <Form />
+
+      
     </div>
   );
 };
---
+++ src/beginners_projects/09_Form_Validation/Form.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import "../09_Form_Validation/style.css";
+const form = () => {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [errorUserName, setErrorUserName] = useState("");
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
+  const [errorConfirmPassword, setErrorConfirmPassword] = useState("");
+
+  const [userColor, setUserColor] = useState("");
+  const [emailColor, setEmailColor] = useState("");
+  const [passwordColor, setPasswordColor] = useState("");
+  const [confirmPasswordColor, setConfirmPasswordColor] = useState("");
+
+  const validate = (e) => {
+    e.preventDefault();
+    if (userName.length < 3) {
+        setErrorUserName("User Name must be at least 3 characters long");
+        setUserColor("red");
+    } else {
+        setErrorUserName("");
+        setUserColor("green");
+    }
+
+    if (!email.includes("@")) {
+        setErrorEmail("Please enter a valid email");
+        setEmailColor("red");
+    } else {
+        setErrorEmail("");
+        setEmailColor("green");
+    }
+
+    if (password.length < 6) {
+        setErrorPassword("Password must be at least 6 characters long");
+        setPasswordColor("red");
+    } else {
+        setErrorPassword("");
+        setPasswordColor("green");
+    }
+
+    if (confirmPassword !== password || confirmPassword === "") {
+        setErrorConfirmPassword("Passwords do not match");
+        setConfirmPasswordColor("red");
+    } else {
+        setErrorConfirmPassword("");
+        setConfirmPasswordColor("green");
+    }
+  };
+
+  return (
+    <>
+      <div className="card">
+        <div className="card-image"></div>
+
+        <form>
+          <input
+            type="text"
+            placeholder="Name"
+            style={{ borderColor: userColor }}
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+
+          <p className="error">{errorUserName}</p>
+
+          <input
+            type="email"
+            placeholder="Email"
+            style={{ borderColor: emailColor }}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <p className="error">{errorEmail}</p>
+
+          <input
+            type="password"
+            placeholder="Password"
+            style={{ borderColor: passwordColor }}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <p className="error">{errorPassword}</p>
+
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            style={{ borderColor: confirmPasswordColor }}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <p className="error">{errorConfirmPassword}</p>
+
+          <button className="submit-btn" onClick={validate}>
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default form;
